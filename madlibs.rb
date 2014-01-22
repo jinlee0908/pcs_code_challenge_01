d459a7481516fcbc95a66bb5ef9ff09dd4a1a44e
AJ Peter <APeter7@gmail.com>
2014-01-21 21:05:35 -0800

    Pushing Increment 2 progress 1/21/14

--- madlibs.rb
+++ madlibs.rb
@@ -1,26 +1,36 @@
 # Increment_1
 print "Please enter a noun or noun phrase"
-noun = gets.chomp
-puts "The #{noun} went to sea in a sturdy ship"
+noun1 = gets.chomp
+puts "The #{noun1} went to sea in a sturdy ship"
 #Increment_2
-def parts_of_speech
-	userinput = Hash.new{
-		noun:,
-		adjective:,
-		verb:,
-		adverb:,
-		object:
-	}
-end
+
+def parts_of_speech 
+
 	print "Please enter a noun"
-	noun = gets.chomp
+	noun2 = gets.chomp
 	print "Please enter an adjective"
 	adjective = gets.chomp
 	print "Please enter a verb"
 	verb = gets.chomp
+	print "Please enter a adverb"
+	adverb = gets.chomp
 	print "Please enter an object"
 	object = gets.chomp
-	puts "#{adjective}, #{noun} #{verb} #{object}"
-	puts userinput
+	
+	 
+
+
+userinput = { :noun => noun2,
+           	  :adjective => adjective,
+              :verb => verb,
+              :adverb => adverb,
+              :object => object
+            }
+
+parts_of_speech {|noun2||verb| puts "#{noun2}, #{verb}"}
+end
+
+puts parts_of_speech
+
 
 
